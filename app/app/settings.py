1997eb986528816081802d40d911ef0d5d2b738f
Astarian123 <marek.idzikowski@comarch.pl>
2022-12-16 18:47:02 +0100

    Added GitHub Actions

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from os import getenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-tdk%$_ir4gap)2nii)e6qyn^4qs+_-in1xtou2iwog(+iwq=6g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
